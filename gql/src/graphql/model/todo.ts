23d5d712fa85fdb9cfabc79a686a67ceb3fdc9cf
Batchuluun0605 <145735591+Batchuluun0605@users.noreply.github.com>
2024-04-03 15:45:31 +0800

    a

---
+++ gql/src/graphql/model/todo.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+const todoSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+    detail: String,
+    date: Date,
+  },
+  { timestamps: true }
+);
+const TodoModel = mongoose.model("todo", todoSchema);
+export default TodoModel;
